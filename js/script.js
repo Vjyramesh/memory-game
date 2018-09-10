62f9ff43be4c6594bd1ca71338785469f7fe50a1
Vijay <vjyramesh@gmail.com>
2018-09-10 16:53:00 +0530

    Design change
    Design and logic change

--- js/script.js
+++ js/script.js
@@ -1,62 +1,113 @@
-var clickCount=0, clickCount1=0;correctCount=0;
-var click, clickModule = {
-    settings:{
-        grid : '.grid'
-    },
-    init: function(option){
-        click = $.extend(this.settings, option);
-        $('body').on('click', click.grid,this.gridActive);
-        $('.row-count, .column-count').on('change', this.displayGrid)
-    },
-    displayGrid: function(){
-        var rowCount = $(".row-count").val();
-        var columnCount = $(".column-count").val();
-        var html=""
-        for(var i=1; i<= rowCount;i++){
-            html+=`<div class="row">`
-            for(var j=1; j<=columnCount;j++){
-                var value = Math.floor(Math.random()*rowCount*columnCount);
-                html+=`<div class="grid" data-id="${value}"></div>`;
-            }
-            html+=`</div>`
-        }
-        $(".game").html(html)
-    },
-    gridActive: function(){
-        clickCount1++;
-        if(!$(this).hasClass('correct') && !$(this).hasClass('active')){
-            var _that = $(this);
-        _that.text(_that.attr('data-id'))
-        ++clickCount;
-        $('.active').each(function(){
-            if($(this).attr('data-id')==_that.attr('data-id')){
-                $(this).addClass('correct').removeClass('active');
-                _that.addClass('correct').removeClass('active');
-                correctCount++;
-                correctValue=1;
-            }
-        })
-        if(clickCount!=2) {
-            _that.addClass('active');
-            clickCount=1;
-        } else {
-            setTimeout(function(){
-                $('.active').text("");
-                if(!_that.hasClass('correct')){
 
-                    _that.text("")
+
+$(document).ready(function() {
+
+var clicked = false;
+
+$(".row-column").on('change', function() {
+    gridGenerator()
+});
+
+$("body").on('click', '.grid', function() {
+        if($(".error-wrapper").length ==0 && $('.grid.shown').length == 0 && !$(this).hasClass("shown-first") && !$(this).hasClass("shown-second") && !$(this).hasClass("success-grid")) {
+            var totalClicks = parseInt($(".total-clicks span").text())+1;
+            console.log($(".total-clicks span").text())
+            $(".total-clicks span").text(totalClicks)
+            clicked = !clicked;
+             if(clicked) {
+            $(this).addClass('shown-first');
+        } else {
+            $(this).addClass('shown-second');
+            if(parseInt($(this).find('span').text()) == parseInt($(".shown-first").find('span').text())) {
+                $(".shown-first, .shown-second").addClass("success-grid");
+                $(".grid").removeClass("shown-first");
+                $(".grid").removeClass("shown-second");
+                var totalCompleted = parseInt($(".total-completed span").text()) + 2;
+                $(".total-completed span").text(totalCompleted);
+                if($(".grid").length === $(".success-grid").length) {
+                    $(".accuracy-rate").addClass("shown");
+                    $(".accuracy-rate").find('span').text(Math.ceil((totalCompleted/totalClicks)*100) + '%');
                 }
-            $('.grid').removeClass('active');
-            }, 2000)
-            clickCount = 0;
+            } else {
+                $(".game-wrapper").addClass("error-wrapper");
+                setTimeout(function() {
+                    $(".grid").removeClass("shown-first");
+                    $(".grid").removeClass("shown-second");
+                    $(".game-wrapper").removeClass("error-wrapper");
+                }, 500);
+            }
         }
+        } else {
+            alert("Select the grid after the numbers get hidden");
         }
-        else {
-            
-            clickCount-1;
+       
+});
+
+
+
+
+function gridGenerator() {
+    var rowItem = parseInt($("#row-select").val());
+    var columnItem = parseInt($('#column-select').val())
+    var rowHTML = '';
+    var gridWrapper = $(".game-grid-wrapper");
+    var gridValues = valueGenerator(rowItem, columnItem);
+    $(".accuracy-rate").removeClass("shown")
+    for(let row =1; row <= (rowItem*columnItem); row++) {
+        if(gridValues.length >= row) {
+         rowHTML +=`<div class="grid shown" style="width: ${100/columnItem}%"><span>${gridValues[row-1]}</span></div>`;
         }
-        $(".correct-count").text(correctCount);
-             $('.total-click').text(clickCount1)
     }
+    gridWrapper.html(rowHTML);
+    setTimeout(function() {
+        $(".grid").removeClass("shown")    
+    }, 1000)
 }
-clickModule.init()
+
+
+
+
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+  var inital= array;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+
+
+
+
+
+
+function valueGenerator(rowItem, columnItem) {
+    var arr = [];
+    while(arr.length < ((rowItem*columnItem)/2)){
+        var randomnumber = Math.floor(Math.random()*100) + 1;
+        if(arr.indexOf(randomnumber) > -1) continue;
+        arr[arr.length] = randomnumber;
+    }
+    var array1 = arr.concat(shuffle(arr))
+    console.log(array1)
+    return array1;
+}
+
+
+
+
+
+
+
+
+
+
+
+gridGenerator();
+});
